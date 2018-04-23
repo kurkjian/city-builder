edfa1aae6c56abcc6f6f5ad107731b829db8e833
kurkjian <kurkjianj@gmail.com>
2018-04-22 20:49:06 -0500

    added images

--- src/Grass.java
+++ src/Grass.java
@@ -4,9 +4,13 @@ public class Grass extends Actor {
 
     public Grass(){
         setImage("img/grass.png");
+        setRotation((int)(Math.random()*4)*90);
     }
 
     public void act() {
-
+        if(Mayflower.mouseClicked(this))
+        {
+            setImage("img/road.png");
+        }
     }
 }
