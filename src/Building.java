f8e7cbe2df204a77be3712aca6cb9f040d3367dd
kurkjian <kurkjianj@gmail.com>
2018-04-25 13:33:39 -0500

    added buildings

---
+++ src/Building.java
@@ -0,0 +1,23 @@
+import mayflower.*;
+
+public class Building extends Actor
+{
+    private int length;
+    private int width;
+    private int cost;
+
+    public Building(int x, int y, int cost)
+    {
+        length = x;
+        width = y;
+        this.cost =cost;
+    }
+
+    public void act()
+    {
+
+    }
+
+
+
+}
